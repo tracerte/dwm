7d59e3cd5996fdbdc03387702be9a1283ccf09e0
Matthew B. Reisch <dev@tracerte.com>
2020-09-24 12:02:29 -0400

    Added keybindings for audio, mic and backlight support

---
+++ xf86keycmds.h
@@ -0,0 +1,7 @@
+static const char *mutecmd[] = { "amixer", "-q", "set", "Master", "toggle", NULL };
+static const char *volupcmd[] = { "amixer", "-q", "set", "Master", "5%+", "unmute", NULL };
+static const char *voldowncmd[] = { "amixer", "-q", "set", "Master", "5%-", "unmute", NULL };
+static const char *miccmd[] = { "amixer", "set", "Capture", "toggle", NULL };
+
+static const char *brupcmd[] = { "xbacklight", "-inc", "10", NULL };
+static const char *brdowncmd[] = { "xbacklight", "-dec", "10", NULL };
